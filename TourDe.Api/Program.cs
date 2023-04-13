9447e1f3d23adb70cb82abcc66c8549404089500
Nathan Dykstra <dykstra.nathan@gmail.com>
2023-04-13 10:45:08 -0500

    updating unit tests for controller pattern; add local appsettings file for developers

--- TourDe.Api/Program.cs
+++ TourDe.Api/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
